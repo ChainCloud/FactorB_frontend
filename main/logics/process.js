4af9342bd1445cac92b8e5e1ca283f6c255025af
Anton Akentiev <anthony.akentiev@gmail.com>
2016-05-12 20:26:13 +0300

    Process task logics

--- main/logics/process.js
+++ main/logics/process.js
@@ -9,13 +9,14 @@ angular.module('factorb.controllers').controller('controllers.ProcessController'
         'ngDialog',
         '$cookies',
         '$q',
+        '$http',
 
         'Upload',
 
         'services.Api',
         'services.Helpers',
 
-        function($scope,$rootScope,$window,$location,$timeout,ngDialog,$cookies,$q,Upload,api,helpers)
+        function($scope,$rootScope,$window,$location,$timeout,ngDialog,$cookies,$q,$http,Upload,api,helpers)
         {
                $scope.reset = function () {
                     $scope.showSpinner = false;
@@ -23,6 +24,7 @@ angular.module('factorb.controllers').controller('controllers.ProcessController'
                     $scope.state = 'Обрабатывается...';
                     $scope.id = '';
                     $scope.sig = '';
+                    $scope.errorMsg = '';
                };
 
                $scope.onGoToMain = function(){
@@ -32,7 +34,85 @@ angular.module('factorb.controllers').controller('controllers.ProcessController'
 
                $scope.load = function(){
                     console.log('Getting task: ' + $scope.id + ' with sig: ' + $scope.sig);
-                     
+                    
+                    // 1 - call process
+                    var url = helpers.getServerUrl() 
+                         + '/tasks/' + $scope.id 
+                         + '/process/v1'
+                         + '?sig=' + $scope.sig;
+
+                    $http({
+                         method:"POST",
+                         data: {},
+                         url: url
+                     })
+                         .success(function (data, status, headers, config) {
+                              $scope.isUploading = false;
+
+                              console.log('Processing called and succeed');
+                              console.log('DATA: ',data);
+
+                              // 2 - check task status
+                              $scope.getTaskStatus();
+                         })
+                         .error(function (data, status, headers, config) {
+                              $scope.isUploading = false;
+
+                              console.log('Can not process task');
+                              $window.alert('Ошибка!'); 
+                         });
+               };
+
+               $scope.getTaskStatus = function(){
+                    console.log('Getting task status');
+                    
+                    var url = helpers.getServerUrl() 
+                         + '/tasks/' + $scope.id 
+                         + '/v1'
+                         + '?sig=' + $scope.sig;
+
+                    $http({
+                         method:"GET",
+                         url: url
+                     })
+                         .success(function (data, status, headers, config) {
+                              $scope.isUploading = false;
+
+                              // 3 - check return 
+                              if(data && data.state==2){
+                                   // complete!
+                                   $scope.processStatus(data);
+                              }else{
+                                   // check again...
+                                   $scope.getTaskStatusAgain();
+                              }
+                         })
+                         .error(function (data, status, headers, config) {
+                              $scope.isUploading = false;
+
+                              console.log('Can not process task');
+                              $window.alert('Ошибка - невозможно получить статус выполнения!'); 
+                         });
+               };
+
+               $scope.getTaskStatusAgain = function(){
+                    var tm = 3000;
+
+                    $timeout(function() {
+                         console.log('Getting task status again...');
+                         $scope.getTaskStatus();
+                    }, tm);
+               };
+
+               // After task is complete -> process data
+               $scope.processStatus = function(data){
+                    console.log('DATA: ',data);
+                    if(data.state!=2){
+                         return;
+                    }
+
+                    $scope.state = 'данные получены!';
+                    $scope.data = data;
                };
 
                /// START:
